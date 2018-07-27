13c183307f5dc12c82527a11be108add5af24e37
curt <curt6815463@gmail.com>
2018-07-27 14:54:57 +0800

    header sytle

--- src/components/HomePage/index.vue
+++ src/components/HomePage/index.vue
@@ -1,8 +1,16 @@
 <template lang="html">
   <div>
     <div class="header">
-      <div class="">
-
+      <div class="header-container">
+        <div class="">
+          <img class="img-word" src="~@/assets/good.png" alt="">
+        </div>
+        <div class="">
+          <img class="img-word" src="~@/assets/ideas.png" alt="">
+        </div>
+        <div class="header-title">
+          想掛號嗎
+        </div>
       </div>
     </div>
   </div>
@@ -22,5 +30,29 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+  .header{
+    background-image: url('~@/assets/header.png');
+    background-size: cover;
+    background-position: center;
+
+    .header-container{
+      width: 50%;
+      margin: 0 auto;
+      padding: 0 5em;
+      text-align: center;
+      background-color: rgba(87, 81, 81,0.4);
+
+      .img-word{
+        width: 100%;
+      }
+
+      .header-title{
+        color: #fff;
+        font-size: 3em;
+        padding: 0.3em 0;
+        font-weight: 600;
+      }
+    }
+  }
 </style>
