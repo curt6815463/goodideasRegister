8f3d1266bcf2f15835e9cce63058d8ebf1a52b9d
curt <curt6815463@gmail.com>
2018-07-27 12:18:10 +0800

    init project

---
+++ src/components/HomePage/index.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div>
+    <div class="header">
+      <div class="">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HomePage',
+  data(){
+    return{
+
+    }
+  },
+  mounted(){
+
+  }
+}
+</script>
+
+<style lang="css">
+</style>
