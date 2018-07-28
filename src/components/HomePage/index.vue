9863c400b53f9c717e10822c36f2c81e5ea009d2
curt <curt6815463@gmail.com>
2018-07-28 14:46:49 +0800

    feat register list style ok

--- src/components/HomePage/index.vue
+++ src/components/HomePage/index.vue
@@ -13,6 +13,34 @@
         </div>
       </div>
     </div>
+    <div class="register-board">
+      <div class="register-board-title">
+        掛號列表
+      </div>
+      <div class="register-lists">
+        <div class="list">
+          <div class="list-image">
+            <img src="http://m1.ablwang.com/uploadfile/2017/0901/20170901042508280.jpg" alt="">
+          </div>
+          <div class="list-info">
+            <div class="info-block">
+              <div class="info-title">
+                切版
+              </div>
+              <div class="info-date">
+                5/23
+              </div>
+              <div class="info-name">
+                kai
+              </div>
+            </div>
+            <div class="info-content">
+              切完第七個版
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -52,6 +80,55 @@ export default {
         font-size: 3em;
         padding: 0.3em 0;
         font-weight: 600;
+      }
+    }
+  }
+  .register-board{
+    width: 70%;
+    margin: 0 auto;
+    margin-top: 3em;
+    .register-board-title{
+      color:#0f375b;
+      font-size: 2em;
+      margin-bottom: 0.7em;
+    }
+    .register-lists{
+      .list{
+        display: flex;
+        .list-image{
+          width: 20%;
+          img{
+            width: 100%;
+            border-radius: 50%;
+          }
+        }
+        .list-info{
+          display: flex;
+          width: 80%;
+          background-color: rgba(30,139,166,.1);
+          .info-block{
+            padding-left: 1em;
+            padding-top: 1em;
+            width: 30%;
+            .info-title{
+              color: #7a062e;
+              font-size: 20px;
+            }
+            .info-date{
+              color: #1e8ba6;
+              font-size: 18px;
+            }
+            .info-name{
+              color: #0f375b;
+              font-size: 20px;
+            }
+          }
+          .info-content{
+            padding-left: 1em;
+            padding-top: 1em;
+            width: 70%;
+          }
+        }
       }
     }
   }
